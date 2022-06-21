e916ea4eb9b7b225c4583a20240d853a31c6c5d9
Marcell Juhasz <marcell.juhasz@nng.com>
2022-06-21 13:44:14 +0200

    sandbox

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Suspense } from "react";
+import "./styles.css";
+import styled from "styled-components";
+
+import Background from "./components/Background";
+import TextSection from "./components/TextSection";
+import Box from "./components/Box";
+import BoxMaterial from "./components/BoxMaterial";
+import Sphere from "./components/AnimatedSphere";
+import Porsche from "./components/Porsche";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+export default function App() {
+  return (
+    <Wrapper className='App'>
+      <Background />
+      <TextSection />
+      <Canvas className='canvas'>
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
+      </Canvas>
+      <Canvas className='canvas'>
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Sphere />
+        </Suspense>
+      </Canvas>
+      <Canvas className='canvas'>
+        <OrbitControls enableZoom={true} autoRotate={true} />
+        <ambientLight intensity={20} position={[1, 10, 1]} />
+        <directionalLight position={[-2, 5, 2]} intensity={4} />
+        <Suspense fallback={null}>
+          <Porsche />
+        </Suspense>
+      </Canvas>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  background: #1f1144;
+
+  canvas {
+    height: 500px;
+  }
+`;
